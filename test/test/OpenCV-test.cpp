245348c70a4d57263b978e84f0b3b183064d65b6
ClivenZ <835105614@qq.com>
2019-06-06 19:18:18 +0800

    code

---
+++ test/test/OpenCV-test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+
+using namespace cv;
+
+int main()
+{
+	// 读入一张图片（poyanghu缩小图）    
+	Mat img = imread("S:\\IMG_4267.JPG");
+	// 创建一个名为 "图片"窗口    
+	namedWindow("图片");
+	// 在窗口中显示图片   
+	cv::imshow("图片", img);
+	// 等待6000 ms后窗口自动关闭    
+	waitKey(6000);
+	return 0;
+}
