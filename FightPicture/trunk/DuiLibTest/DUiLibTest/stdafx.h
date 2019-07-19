a2826ed39ab5599d2299f9eb642d300065c1c655
ClivenZ <835105614@qq.com>
2019-07-19 18:04:33 +0800

    Duilib_Test

--- FightPicture/trunk/DuiLibTest/DUiLibTest/stdafx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "UIlib.h"
-using namespace DuiLib;
-#ifdef _DEBUG
-#pragma comment(lib, "../../lib/Duilib_d.lib")
-#else
-#pragma comment(lib, "../../lib/Duilib.lib")
-#endif
--- String_Test/String_Test/test.cpp
+++ String_Test/String_Test/test.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 
 int main() {
+	string s = "HelloWorld";
 
+	
 
 	return 0;
 }
