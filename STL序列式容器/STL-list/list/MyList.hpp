b840d18b95085083744c24e4a700f58e8de71497
ClivenZ <835105614@qq.com>
2019-08-20 22:08:25 +0800

    “MyList”

--- STL序列式容器/STL-list/list/MyList.hpp
+++ STL序列式容器/STL-list/list/MyList.hpp
@@ -1,1 +1,277 @@
 #pragma once
+#include<iostream>
+using namespace std;
+namespace My {
+	template<class T>
+	struct ListNode{
+		ListNode(const T& val = T())
+			:_pPre(nullptr)
+			, _pNext(nullptr)
+			, _val(nullptr)
+		{}
+		ListNode<T>* _pPre;
+		ListNode<T>* _pNext;
+		T _val;
+	};
+	//正向迭代器
+	template<class T, class Ref, class Ptr>
+	class ListIterator {
+		typedef ListNode<T>* PNode;
+		typedef ListIterator<T, Ref, Ptr> Self;
+	public:
+		ListIterator(PNode pNode = nullptr)
+			:_pNode(pNode)
+		{}
+		ListIterator(const Self& l)
+			:_pNode(l._pNode) 
+		{}
+		T* operator*() {
+			return _pNode->_val;
+		}
+		T* operator->() {
+			return &(operator*());
+		}
+		Self& operator++() {
+			_pNode = _pNode->_pNext;
+			return *this;
+		}
+		Self& operator++(int) {
+			Self temp(*this);
+			_pNode = _pNode->_pNext;
+			return temp;
+		}
+		bool operator==(const Self& l) {
+			return _pNode == l._pNode;
+		}
+		bool operator!=(const Self& l) {
+			return _pNode != l._pNode;
+		}
+		PNode _pNode;
+	};
+	//反向迭代器
+	template<class T,class Ref,class Ptr,class Iterator>
+	class ListReverseIterator {
+		typedef ListReverseIterator<T, Ref, Ptr, iterator> Self;
+	public:
+		ListReverseIterator(const Iterator& it)
+			:_it(it)
+		{}
+		ListReverseIterator(const Self& l)
+			:_it(l._it)
+		{}
+		Ref operator*() {
+			Iterator temp = _it;
+			return *(--temp);
+		}
+		Ref operator->() {
+			return &operator*();
+		}
+		Self& operator++() {
+			--_it;
+			return *this;
+		}
+		Self operator++() {
+			Iterator temp(_it);
+			--_it;
+			return temp;
+		}
+		Self& operator--() {
+			++_it;
+			return *this;
+		}
+		Self operator--() {
+			iterator temp(_it);
+			++_it;
+			return temp;
+		}
+		bool operator!=(const Self& s) {
+			return _it != s._it;
+		}
+		bool operator==(const Self& s) {
+			return _it == s._it;
+		}
+	private:
+		Iterator _it;
+	};
+	template<class T>
+	class List {
+		typedef ListIterator<T, T&, T*> Iterator;
+		typedef ListIterator<T, const T&, const T*> ConstIterator;
+		typedef ListReverseIterator<T, T&, T*, Iterator> ReverseIterator;
+		typedef ListReverseIterator<T, const T&, const T*, const Iterator> ConstReveresIterator;
+	public:
+		//构造
+		List() {
+			CreateHead();
+		}
+		List(int n ,const T& value = T()) {
+			CreateHead();
+			for (int i = 0; i < n;++i) {
+				PushBack(value);
+			}
+		}
+		template<class Iterator>
+		List(Iterator first,Iterator last) {
+			CreateHead();
+			while (first!=last) {
+				PushBack(*first);
+				++first;
+			}
+		}
+		List(const List<T>& l) {
+			CreateHead();
+			List<T> temp(l.CBegin(), l.CEnd());
+			this->Swap(temp);
+		}
+		List<T>& operator=(const List<T>& l) {
+			if (this != &l) {
+				List<T> temp(l);
+				this->Swap(temp);
+			}
+			return *this;
+		}
+		~List() {
+			Clear();
+			delete _PHead;
+			_PHead = nullptr;
+		}
+		/*    List Itreator   */
+		Iterator Begin() {
+			return Iterator(_PHead->_pNext);
+		}
+		Iterator End() {
+			return Iterator(_PHead);
+		}
+		ReverseIterator RBegin() {
+			return ReverseIterator(End());
+		}
+		ReverseIterator REnd() {
+			return ReverseIterator(Begin());
+		}
+		ConstIterator CBegin() {
+			return ConstIterator(_PHead->_pNext);
+		}
+		ConstIterator CEnd() {
+			return ConstIterator(_PHead);
+		}
+		ConstReveresIterator CRBegin() {
+			return ConstReveresIterator(CEnd());
+		}
+		ConstReveresIterator CREnd() {
+			return ConstReveresIterator(CBegin());
+		}
+		/*    List Capacity   */
+		size_t Size()const {
+			size_t count = 0;
+			PNode pCur = _PHead->_pNext;
+			while (pCur != _PHead) {
+				++count;
+				pCur = pCur->_pNext;
+			}
+			return count;
+		}
+		bool Empty() {
+			return _PHead->_pNext == _PHead;
+		}
+		void Resize(size_t newSize,const T& val = T()) {
+			size_t oldSize = Size();
+			if (oldSize <= newSize) {
+				for (size_t i = oldSize; i < newSize; ++i)
+					PushBack(val);
+			}
+			else {
+				for (size_t i = newSize; i < oldSize; ++i)
+					PopBack(val);
+			}
+		}
+		/*    List Access   */
+		T& Front() {
+			return _PHead->_pNext->_val;
+		}
+		const T& Front()const {
+			return _PHead->_pNext->_val;
+		}
+		T& Back() {
+			return _PHead->_pPre->_val;
+		}
+		const T& Back() const {
+			return _PHead->_pPre->_val;
+		}
+		/*    List Modify   */
+		void PushBack(const T& val) {
+			PNode pNewNdoe = new Node(val);
+
+			pNewNdoe->_pNext = _PHead;
+			pNewNdoe->_pPre = _PHead->_pPre;
+
+			_PHead->_pPre = pNewNdoe;
+			pNewNdoe->_pPre->_pNext = pNewNdoe;
+		}
+		void PopBack() {
+			PNode pDel = _PHead->_pPre;
+			if (pDel != _PHead) {
+				_PHead->_pPre = pDel->_pPre;
+				pDel->_pPre->_pNext = _PHead;
+				delete pDel;
+			}
+		}
+		void PushFront(const T& val) {
+			PNode pNewNode = new Node(val);
+
+			pNewNode->_pNext = _PHead->_pNext;
+			pNewNode->_pPre = _PHead;
+
+			_PHead->_pNext = pNewNode;
+			pNewNode->_pNext->_pPre = pNewNode;
+		}
+		void PopFront() {
+			PNode PDel = _PHead->_pNext;
+			if (PDel != _PHead) {
+				_PHead->_pNext = PDel->_pNext;
+				PDel->_pNext->_pPre = _PHead;
+				delete PDel;
+			}
+		}
+		Iterator Inerst(Iterator pos,const T& val) {
+			PNode pNewNode = new Node(val);
+			PNode pCur = pos._pNode;
+
+			pNewNode->_pPre = pCur->_pPre;
+			pNewNode->_pNext = pCur;
+			pNewNode->_pPre->_pNext = pNewNode;
+			pCur->_pPre = pNewNode;
+			return iterator(pNewNode);
+		}
+		Iterator Erase(Iterator pos) {
+			PNode pDel = pos._pNode;
+			PNode pRet = pDel->_pNext;
+
+			pDel->_pPre->_pNext = pDel->_pNext;
+			pDel->_pNext->_pPre = pDel->_pPre;
+			delete pDel;
+
+			return Iterator(pRet);
+		}
+		void Clear() {
+			PNode pCur = _PHead->_pNext;
+			while (pCur != _PHead) {
+				_PHead->_pNext = pCur->_pNext;
+				delete pCur;
+				pCur = _PHead->_pNext;
+			}
+			_PHead->_pNext = _PHead;
+			_PHead->_pPre = _PHead;
+		}
+		void Swap(List<T>& l) {
+			swap(_PHead,l._PHead);
+		}
+	private:
+		void CreateHead() {
+			_PHead = new Node;
+			_PHead->_pPre = _PHead;
+			_PHead->_pNext = _PHead;
+		}
+	private:
+		PNode _PHead;
+	};
+}
--- STL序列式容器/STL-list/list/main.cpp
+++ STL序列式容器/STL-list/list/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<list>
-
+#include<vector>
 using namespace std;
 
 void MakeListTest() {
@@ -24,8 +24,187 @@ void MakeListTest() {
 		cout << x << " ";
 	cout << endl;
 }
+void IteratorListTest() {
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	list<int> ll(array, array + sizeof(array) / sizeof(int));
+
+	//正向迭代器
+	for (list<int>::iterator it = ll.begin(); it != ll.end();++it) {
+		cout << *it << " ";
+	}
+	cout << endl;
+	//反向迭代器
+	for (list<int>::reverse_iterator it = ll.rbegin(); it != ll.rend();++it) {
+		cout << *it << " ";
+	}
+	cout << endl;
+	//const 正向迭代器(C++11)
+	auto cit = ll.cbegin();
+	cout << typeid(cit).name() << endl;
+	//const 反向迭代器(C++11)
+	auto crit = ll.crbegin();
+	cout << typeid(crit).name() << endl;
+}
+void ListCapacityTest() {
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	list<int> ll(array, array + sizeof(array) / sizeof(int));
+
+	//打印list 中有效节点的个数
+	cout << ll.size() << endl;
+
+	//检测list 是否为空
+	if (ll.empty())
+		cout << "empty()" << endl;
+	else
+		for (auto a : ll)
+			cout << a << " ";
+		cout << endl;
+}
+void ListElementAccessTest() {
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	list<int> ll(array, array + sizeof(array) / sizeof(int));
+
+	for (auto a : ll)
+		cout << a << " ";
+	cout << endl;
+
+	//将list 中的第一个节点的值 和 最后一个节点的值 改为 20；
+	ll.front() = 20;
+	ll.back() = 20;
+
+	for (auto a : ll)
+		cout << a << " ";
+	cout << endl;
+
+	const list<int> ll2(array,array+sizeof(array)/sizeof(int));
+	const int& ca = ll2.front();
+
+}
+void ListIteratorError() {
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	list<int> ll(array, array + sizeof(array) / sizeof(int));
+
+	auto it = ll.begin();
+	while (it != ll.end()) {
+		//erase()执行后，it 所指向的节点已经删除，因此 it 无效，在下一次使用 it 时，必须先给其赋值
+		ll.erase(it);
+		++it;
+	}
+}
+void Print(list<int>& l) {
+	for (auto a : l)
+		cout << a << " ";
+	cout << endl;
+}
+
+class Date {
+public:
+	Date(int year = 1900, int mouth = 1, int day = 1)
+		:_year(year)
+		, _mouth(mouth)
+		, _day(day)
+	{
+		cout << "Date(int,int,int):" << this << endl;
+	}
+	Date(const Date& d)
+		:_year(d._year)
+		, _mouth(d._mouth)
+		, _day(d._day)
+	{
+		cout << "Date(const Date&):" << this << endl;
+	}
+	~Date() {
+
+	}
+private:
+	int _year;
+	int _mouth;
+	int _day;
+};
+void ListModifiersTest() {
+	int array1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	list<int> L(array1, array1 + sizeof(array1) / sizeof(int));
+
+	// push_front / pop_front / push_back / pop_back
+	L.push_front(6);
+	L.push_back(6);
+	Print(L);
+
+	L.pop_front();
+	L.pop_back();
+	Print(L);
+
+	//emplace_back / emplace_front / empalce
+	list<Date> l;
+	Date d(2018, 1, 1);
+	l.push_back(d);
+	l.emplace_back(2012, 12, 12);
+	l.emplace_front(2019, 12, 12);
+
+	//insert / erase
+	int array[] = { 1, 2, 3};
+	list<int> L1(array, array + sizeof(array) / sizeof(int));
+	//获取链表的第二个结点
+	auto pos = ++L1.begin();
+	cout << *pos << endl;
+	//在pos的前面插入4
+	L1.insert(pos, 4);
+	Print(L1);
+	//在pos的前插入4个5
+	L1.insert(pos, 4, 5);
+	Print(L1);
+	//在pos前面插入[v.begin(),v.end())区间中的的元素
+	vector<int> v{9,5,2,7};
+	L1.insert(pos, v.begin(), v.end());
+	Print(L1);
+
+
+	//删除pos位置的元素
+	L1.erase(pos);
+	Print(L1);
+	//删除list 中[begin,end)区间的中的所有元素
+	L1.erase(L1.begin(), L1.end());
+	Print(L1);
+
+
+
+}
+
+void Test() {
+	int array[] = { 1, 2, 3 };
+	list<int> L(array, array + sizeof(array) / sizeof(int));
+	Print(L);
+	
+	//L的元素增加为10个，多余的默认值填充
+	L.resize(10);
+	Print(L);
+	//L的元素增加为20个，多余的用1填充
+	L.resize(20, 1);
+	Print(L);
+	//L的元素减少到5个
+	L.resize(5);
+	Print(L);
+	//用vector中的元素来构造list
+	vector<int> v{ 9,5,2,7 };
+	list<int> ll(v.begin(), v.end());
+	Print(ll);
+	//交换L和ll
+	L.swap(ll);
+	Print(L);
+	Print(ll);
+	//清空ll
+	Print(ll);
+
+}
+
 
 int main() {
-	MakeListTest();
+	//MakeListTest();
+	//IteratorListTest();
+	//ListCapacityTest();
+	//ListElementAccessTest();
+	//ListIteratorError();
+	//ListModifiersTest();
+	//Test();
 	return 0;
 }
